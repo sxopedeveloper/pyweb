e77d0cacdb20b1fab9b85da146f758067c1dabb2
annbalarachell <annbalarachell@gmail.com>
2021-05-11 14:42:28 +0530

    Inittial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    app.route('/')
+    return render_template("home.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+@app.route('/conf/')
+def conf():
+    return render_template("conf.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
